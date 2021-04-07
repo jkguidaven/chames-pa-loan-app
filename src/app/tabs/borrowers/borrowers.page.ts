8deefb4528e1290752141dab6ed68602b7ae2ec9
James Kenneth A. Guidaven <jkguidaven@gmail.com>
2021-04-07 20:24:22 +0800

    Add main tabs

---
+++ src/app/tabs/borrowers/borrowers.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-borrowers',
+  templateUrl: './borrowers.page.html',
+  styleUrls: ['./borrowers.page.scss'],
+})
+export class BorrowersPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
